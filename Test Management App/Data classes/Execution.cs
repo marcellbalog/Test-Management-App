95de584ccf32028d6083012e766a80917d3fc567
marcellbalog <50045517+marcellbalog@users.noreply.github.com>
2023-04-24 19:34:06 +0200

    Add Execution model class & user control

---
+++ Test Management App/Data classes/Execution.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+class Execution
+{
+	public int ID { get; set; }
+	public int TestID { get; set; }
+	public DateTime Date { get; set; }
+	public int Time { get; set; }
+	public int Result { get; set; }
+	public int FailedStepID { get; set; }
+	public string Comment { get; set; }
+
+}
+
